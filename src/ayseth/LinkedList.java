acff19808aa6f604cf5cdcc5e024d5cb3c322879
ayushseth12 <ayushseth12@gmail.com>
2017-02-07 11:57:38 +0530

    LinkList

--- src/ayseth/LinkedList.java
+++ src/ayseth/LinkedList.java
@@ -56,6 +56,47 @@ public class LinkedList {
             current=current.getNext();
         }
     }
+    //Finding nth node from end.
+    int nthnode(LinkedList head,int pos){
+        if(head==null){
+            System.out.println("Linked list is empty");
+            return 0;
+        }
+        LinkedList node1=head;
+        LinkedList node2=head;
+        int i=1;
+        while(i!=pos){
+            node1=node1.getNext();
+            i++;
+        }
+        while (node1.getNext()!=null){
+
+            node1=node1.getNext();
+            node2=node2.getNext();
+        }
+        return node2.getData();
+    }
+
+    //reverse a Linked List
+    LinkedList reverse(LinkedList head){
+
+        if(head==null){
+            System.out.println("The Linked List is Empty");
+            return null;
+        }
+        LinkedList node1=head;
+        LinkedList node2=null;
+        LinkedList node3=null;
+        while(node1!=null){
+            node3=node1.getNext();
+            node1.setNext(node2);
+            node2=node1;
+            node1=node3;
+        }
+        return node2;
+    }
+
+
     public static void main(String[] args){
 
         LinkedList head=new LinkedList(0);
@@ -66,6 +107,12 @@ public class LinkedList {
         head=head.insert(head,25);
         head=head.insert(head,30);
         head.printlist(head);
+        int i=head.nthnode(head,3);
+        System.out.println("The 3rd from end is "+i);
+        head=head.reverse(head);
+        head.printlist(head);
+        i=head.nthnode(head,3);
+        System.out.println("The 3rd from end is "+i);
     }
 
 
